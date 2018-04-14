e7236693d1488028105551f544be9a17e432f075
g-snowhawk <githave@p5on.net>
2018-04-14 10:03:01 +0900

    Add Tms\security
    - Support exec plugin after the authentication
    - Update submodule

---
+++ lib/Tms/Security.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * This file is part of P5 Framework.
+ *
+ * Copyright (c)2018 PlusFive (https://www.plus-5.com)
+ *
+ * This software is released under the MIT License.
+ * https://www.plus-5.com/licenses/mit-license
+ */
+
+namespace Tms;
+
+/**
+ * Methods for Authentication.
+ *
+ * @license  https://www.plus-5.com/licenses/mit-license  MIT License
+ * @author   Taka Goto <www.plus-5.com>
+ */
+class Security extends \P5\Security
+{
+    /**
+     * Application instance
+     *
+     * @var Tms\App
+     */
+    private $app;
+
+    /**
+     * Object constructer.
+     *
+     * @param string $source
+     * @param P5\Db  $db
+     */
+    public function __construct($source, Db $db = null, $password_algo = 'sha1')
+    {
+        foreach (debug_backtrace() as $trace) {
+            if (isset($trace['object']) && $trace['object'] instanceof App) {
+                $this->app = $trace['object'];
+                break;
+            }
+        }
+
+        parent::__construct($source, $db, $password_algo);
+    }
+
+    /**
+     * Check User/Password.
+     *
+     * @param string $uname
+     * @param string $upass
+     * @param string $secret
+     *
+     * @return bool
+     */
+    public function authentication($uname, $upass, $secret = '')
+    {
+        $authorized = parent::authentication($uname, $upass, $secret);
+
+        $results = $this->app->execPlugin('afterAuthentication', $uname, $authorized);
+        foreach ($results as $result) {
+            if ($result === false) {
+                $authorized = false;
+                break;
+            }
+        }
+
+        return $authorized;
+    }
+}
